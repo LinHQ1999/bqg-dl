acb3dd99c61ebaebca69c981c894476b9379c62c
LinHQ1999 <1157136867@qq.com>
2021-08-12 19:52:23 +0800

    更醒目的进度条

--- utils/load_bar.go
+++ utils/load_bar.go
@@ -16,6 +16,11 @@ type Bar struct {
 	length  int
 }
 
+var (
+	l = color.New(color.BgHiGreen).SprintFunc()
+	r = color.New(color.BgHiRed).SprintFunc()
+)
+
 // NewBar 获取进度条实例
 func NewBar(total int32, length int) *Bar {
 	return &Bar{
@@ -31,9 +36,9 @@ func (bar *Bar) AddAndShow(delta int32) {
 	// 避免脏读，最好使用返回的值
 	div := float32(now) / float32(bar.total)
 	loaded := int(div * float32(bar.length))
-	fmt.Fprintf(color.Output, "进度: <%s%s> %.2f%%\r",
-		color.HiGreenString(strings.Repeat("=", loaded)),
-		strings.Repeat("-", bar.length-loaded),
+	fmt.Fprintf(color.Output, "进度: |%s%s| %.2f%%\r",
+		l(strings.Repeat(" ", loaded)),
+		r(strings.Repeat(" ", bar.length-loaded)),
 		div*100,
 	)
 }
